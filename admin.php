fe0e7193c4e4008ae856ee707e39f1111d86272f
TheMikaMan <47526162+TheMikaMan@users.noreply.github.com>
2019-04-12 11:46:07 +0200

    update

--- admin.php
+++ admin.php
@@ -54,43 +54,52 @@ if(isset($_GET['page']) && is_numeric($_GET['page'])) {
 			}
 			$offset = ($page - 1) * $rowsperpage;
 
-$sql = "SELECT * FROM klant ORDER BY id DESC LIMIT $offset, $rowsperpage";
+$sql = "SELECT * FROM klant ";
 $result = mysqli_query($dbcon, $sql);
 
-if(mysqli_num_rows($result) < 1) {
-    echo "No post found";
-	}
+//if(mysqli_num_rows($result) < 1) {
+//    echo "No post found";
+//	}
 
+//    ORDER BY id DESC LIMIT $offset, $rowsperpage
+
+echo "<table>";
+echo "<tr>";
 echo "<th>Klant id</th>";
 echo "<th></th>";
+echo "<th>Voornaam</th>";
+echo "<th>Achternaam</th>";
+echo "<th></th>";
 echo "<th>Date</th>";
-echo "<th>Views</th>";
-echo "<th>Action</th>";
 echo "</tr>";
 	while ($row = mysqli_fetch_assoc($result)) {
-		$id = $row['id'];
-        $naam = $row['naam'];
-        $tvoeg = $row['tvoeg'];
-        $achtnaam = $row ['achtnaam'];
-        $gesla = $row ['gesla'];
-        $telnum = $row ['telnum'];
-        $email = $row ['email'];
-        $straat = $row ['straat'];
-        $huisnum = $row ['huisnum'];
-        $post = $row ['post'];
-        $woon = $row ['woon'];
+		$id = $row['klant_id'];
+        $naam = $row['voornaam'];
+        $tvoeg = $row['tussenvoegsel'];
+        $achtnaam = $row ['achternaam'];
+        $gesla = $row ['geslacht'];
+        $telnum = $row ['tel.nummer'];
+        $email = $row ['e-mail'];
+        $straat = $row ['straatnaam'];
+        $huisnum = $row ['huisnummer'];
+        $post = $row ['postcode'];
+        $woon = $row ['woonplaats'];
         $land = $row ['land'];
         $date = $row ['aanmaakdatum'];
-        $id = $_POST['id'];
+//        $id = $_POST['id'];
       ?>
+<?php
+echo "<tr>
+	<td>$id</td>
+<td><a class='name' href='view.php?id=$id'>$naam</a></td>
+<td><a class='achtnaam' href='view.php?id=$id'>$achtnaam</a></td>
+<td><a class='date'>$date</a></td>
+<td></td>
+<td><a class='editadmin' href='edit.php?id=$id'>Edit</a><a class='deleteadmin' href='del.php?id=$id' onclick='return confirm('Are you sure you want to delete this post?')'>Delete</a></td>
+</tr>"
 
-<tr>
-	<td><?php echo $id;?></td>
-<td><a href="view.php?id=<?php echo $id;?>"><?php echo substr($title) ;?></a></td>
-<td><?php echo $time;?></td>
-<td><?php echo $hits;?></td>
-<td><a href="edit.php?id=<?php echo $id;?>">Edit</a> | <a href="del.php?id=<?php echo $id;?>" onclick="return confirm('Are you sure you want to delete this post?')">Delete</a></td>
-</tr>
+?>
+
 
 <?php
 }
